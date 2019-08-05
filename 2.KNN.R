6be7a6fccf47464b22ce95a90c8dd0d6844e9520
gomguard <knholic@gmail.com>
2019-08-05 18:43:56 +0900

    Data Preprocess KNN algorithm

--- 2.KNN.R
+++ 2.KNN.R
@@ -25,8 +25,43 @@ normalize <- function(x){
   return((x - min(x)) / (max(x) - min(x)))
 }
 
-# https://rfriend.tistory.com/52 
-1:50 %>% 
-  normalize()
-seq(0,50,5) %>% 
-  normalize()
+
+wbcd_n <- wbcd %>% 
+  dplyr::select(-1,-2) %>% 
+  map( normalize) %>% 
+  as_tibble()
+
+wbcd_n %>% summary()
+
+wbcd_train <- wbcd_n[1:469, ]
+wbcd_test  <- wbcd_n[470:569, ]
+
+wbcd_train_lbls <- wbcd[1:469, 2]
+wbcd_test_lbls <- wbcd[470:569, 2]
+
+library(class)
+library(gmodels)
+
+
+wbcd_n <- wbcd %>% 
+  dplyr::select(-1, -2) %>% 
+  map(scale) %>% 
+  as_tibble()
+
+result <- tibble()
+for (idx in 1:50) {
+  wbcd_test_pred <- knn(train = wbcd_train, test = wbcd_test, 
+                        cl = wbcd_train_lbls %>% pull(), k = idx)  
+  
+  ct <- CrossTable(x = wbcd_test_lbls %>% pull(), y = wbcd_test_pred)
+  
+  ct_prop <- ct$prop.tbl
+  prop_wrong <- ct_prop[2,1] + ct_prop[1,2]
+  result <- result %>% 
+    bind_rows(list(idx = idx, value = prop_wrong))
+}
+par(mfrow = c(1, 1))
+plot(result)
+
+result %>% 
+  dplyr::filter(value == min(value))
