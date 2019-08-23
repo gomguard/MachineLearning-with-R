67acc74629a53e53994f4beb581cfab3ef1a046d
gomguard <knholic@gmail.com>
2019-08-23 22:30:14 +0900

    install package in lower R version

---
+++ etc/how_to_install_package_low_version.R
@@ -0,0 +1,5 @@
+# https://bluebead38.blogspot.com/2017/07/r-cran.html
+install.packages("https://cran.r-project.org/src/contrib/Archive/mvtnorm/mvtnorm_1.0-8.tar.gz", 
+                 repos = NULL, type="source")
+
+# https://cran.r-project.org/web/packages/[패키지명]/index.html
