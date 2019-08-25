7011bb18b0038f45a93e42a90f0c21cf2a98bce2
gomguard <knholic@gmail.com>
2019-08-25 23:10:47 +0900

    chrome desktop

---
+++ etc/chrome_desktop_in_ubuntu.R
@@ -0,0 +1,12 @@
+
+# https://ksw151515.tistory.com/16
+
+# cd ./Downloads
+# 
+# sudo apt-get install ca-certificates
+# 
+# sudo apt-get install libappindicator1
+# 
+# sudo apt-get install xfce4
+# 
+# Home폴더에 가서 메모장으로 .chrome-remote-desktop-session을 만듭니다. 그리고 내용으로 startxfce4 를 적어 줍니다. 
