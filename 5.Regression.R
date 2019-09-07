cbb3abcdea6bb9efdaf054e6400ad92202aaf16f
gomguard <knholic@gmail.com>
2019-09-08 03:03:45 +0900

    commit

--- 5.Regression.R
+++ 5.Regression.R
@@ -65,3 +65,32 @@ lm(expenses ~ ., data = insurance)
 
 library(psych)
 pairs.panels(insurance[c('age', 'bmi', 'children', 'expenses')])
+
+# 타원 객체 - 상관관계 타원형 - 늘어질 수록 관계 강함
+# 곡선 - 뢰스 곡선 - 추세선 - age:children 같은 경우는 볼만함
+
+ins_model <- lm(expenses ~ age + sex + smoker + children + bmi + region,
+                data = insurance)
+
+# how to save and load linear model 
+ins_model <- lm(expenses ~ ., data = insurance)
+saveRDS(ins_model, './lm_test.rds')
+aaa <- readRDS('./lm_test.rds')
+
+# 모델에서 각각의 계수는 다른 값들이 고정됐다고 가정했을 때를 기준으로 계산된다.
+# 그렇기 때문에 독립성이 중요함
+# 만약 각각이 독립적이지 않다면 한 값이 변화할 때 다른 값이 고정되는 것이 불가능하기 때문에 잘못된 값을 도출할 수 있음
+
+summary(ins_model)
+
+# Part 1. Residuals : 실제값 - 예측값
+ins_model$residuals %>% 
+  plot()
+
+# Part 2. p-value : 추정계수가 실제 0일 확률 추정치
+# 작을 경우 실제 계수가 0이 아닐 가능성이 높다는 것이며 특징이 종속 변수와 관계 없을 가능성이 매우 낮다는 것을 의미
+# 유의 수준 보다 낮은 p value 는 통계적으로 유의한 것으로 간주 (statistically significant)
+# 유의 수준 : 0.05
+
+
+
