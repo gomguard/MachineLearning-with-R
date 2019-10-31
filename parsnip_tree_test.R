e795dc4e1766d94226d9b5c9fd80918956d5c75a
gomguard <knholic@gmail.com>
2019-11-01 01:03:42 +0900

    parsnip comment

--- parsnip_tree_test.R
+++ parsnip_tree_test.R
@@ -36,3 +36,17 @@ mod_xgb %>%
 
 mape(result, mpg, .pred_rf)
 mape(result, mpg, .pred_xg)
+
+# xgboost parameter
+# https://tidymodels.github.io/parsnip/reference/boost_tree.html
+# boost_tree() is a way to generate a specification of a model before fitting and allows the model to be created using different packages in R or via Spark. The main arguments for the model are:
+#   
+# mtry: The number of predictors that will be randomly sampled at each split when creating the tree models.
+# trees: The number of trees contained in the ensemble. 
+# min_n: The minimum number of data points in a node that are required for the node to be split further. 
+# tree_depth: The maximum depth of the tree (i.e. number of splits). 
+# learn_rate: The rate at which the boosting algorithm adapts from iteration-to-iteration. 
+# loss_reduction: The reduction in the loss function required to split further. 
+# sample_size: The amount of data exposed to the fitting routine.
+
+
