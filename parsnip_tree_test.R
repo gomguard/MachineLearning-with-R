8374666603f4192b02d6db7a744a8c8f8691a336
gomguard <34765216+gomguard@users.noreply.github.com>
2019-11-01 15:14:13 +0900

    Update parsnip_tree_test.R

--- parsnip_tree_test.R
+++ parsnip_tree_test.R
@@ -50,3 +50,77 @@ mape(result, mpg, .pred_xg)
 # sample_size: The amount of data exposed to the fitting routine.
 
 
+
+
+library(tidyverse)
+library(tidymodels)
+library(DBI)
+library(RPostgreSQL)
+library(parsnip)
+
+mod_rf <- function(.x, .log, .trees = 2000, .mtry = 5, .mode = 'regression'){
+  print(sprintf('RandomForest : %s', .log))
+  mod <- rand_forest(trees = .trees, mtry = .mtry, mode = .mode) %>% 
+    set_engine('ranger', seed = 1101) %>% 
+    fit(so_qty ~ ., .x)
+  
+  return(mod)
+}
+
+
+mod_xg <- function(.x, .log, .trees = 2000, .mtry = 5, .mode = 'regression'){
+  print(sprintf('XGBoost : %s', .log))
+  mod <- boost_tree(mode = .mode, mtry = .mtry, trees = .trees) %>% 
+    set_engine('xgboost') %>% 
+    update() %>% 
+    fit(so_qty ~ ., .x)
+  
+  return(mod)
+}
+
+#### main ####
+aggr_tbl_s <- base_tbl %>% 
+  mutate(actualyear = str_sub(actualweek, 1, 4),
+         lnch_flag = if_else(actualweek_num == lnch_week_num, 1, 0)) %>% 
+  select(-c(attb06, productgroup, lnch_week, actualweek, lnch_week_num)) %>% 
+  group_by(orglevel1, orglevel2, countrygroup) %>% 
+  nest()
+
+rec <- aggr_tbl_s$data[[1]] %>% 
+  recipe(so_qty ~ .) %>% 
+  step_center(all_numeric(), -so_qty) %>% 
+  step_scale(all_numeric(), -so_qty) %>% 
+  step_dummy(all_nominal(), one_hot = T) %>% 
+  prep()
+
+# data split
+split_data <- function(.x, .type){
+  tryCatch({
+    result <- .x %>% 
+    {if(.type == 'test') dplyr::filter(., productnm == target_model) else dplyr::filter(., productnm != target_model)} %>% 
+      bake(rec, .)  
+    return(result)
+  }, error = function(e){
+    print(e)
+    return(list())
+    })
+}
+
+target_model <- 'S10 TTL'
+aggr_tbl_s$data[[1]]$productnm %>% unique() 
+aggr_tbl_s$data[[1]] %>% 
+  dplyr::filter(productnm == target_model)
+
+aggr_tbl_s_lv1 <- aggr_tbl_s %>% 
+  mutate(train = map(data, ~split_data(.x, .type = 'train'))) %>% 
+  mutate(test  = map(data, ~split_data(.x, .type = 'test')))
+
+
+
+# fit model
+fit <- aggr_tbl_s_lv1 %>% 
+  mutate(.pred_rf = map2(data, countrygroup, mod_rf),
+         .pred_xg = map2(data, countrygroup, mod_xg))
+
+
+
