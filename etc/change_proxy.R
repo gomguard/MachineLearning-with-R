b23ed1272f453f6b93f4e0378e332b96caca1ab7
gomguard <34765216+gomguard@users.noreply.github.com>
2019-11-14 11:05:24 +0900

    Update change_proxy.R

--- etc/change_proxy.R
+++ etc/change_proxy.R
@@ -13,3 +13,19 @@ double-check this with:
 
 Sys.getenv("http_proxy")
 I am behind university proxy, and this solution worked perfectly. The major issue is to export the items in Terminal before running R, both in upper- and lower-case.
+
+
+###############################
+
+library(httr)
+library(rvest)
+Sys.setenv(http_proxy =  "http://168.219.61.252:8080/")
+Sys.getenv('http_proxy')
+R.home("etc")
+file.exists(file.path(R.home("etc"), "curl-ca-bundle.crt"))
+
+curl:::.onLoad()
+[1] "C:/PROGRA~1/R/R-36~1.0/etc/curl-ca-bundle.crt"
+"C:\Program Files\R\R-3.6.0\etc\curl-ca-bundle.crt"
+
+read_html('http://www.naver.com')
