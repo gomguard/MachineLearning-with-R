d573ff54c93921f65bb034fec4a1b473de1674f1
gomguard <knholic@gmail.com>
2019-11-25 17:06:23 +0900

    parallel

--- crawl_gis.R
+++ crawl_gis.R
@@ -7,8 +7,8 @@ base_url <- 'http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/
 base_grid <- expand.grid(x = -180:180,y = -90:90)
 {
 st <- Sys.time()
-result <- base_grid %>% 
-  sample_n(100) %>% 
+base_tbl_one <- base_grid %>% 
+  sample_n(100) %>%
   mutate(api_url = sprintf(base_url, x, y)) %>% 
   as_tibble() %>% 
   mutate(addr = map(api_url, get_addr))
@@ -40,10 +40,47 @@ get_addr <- function(.base_url) {
 
 library(foreach)
 library(doParallel)
-result %>% 
-  unnest()
+
+base_tbl <- base_tbl %>% 
+  mutate(grp = row_number() %% 11 + 1) 
+
+
 numcores <- detectCores() - 1
 mycluster <- makeCluster(numcores)
 registerDoParallel(mycluster)
 
 record <- tibble()
+clusterExport(mycluster, 'record')
+{
+st <- Sys.time()
+result_pr <- foreach(.combine = bind_rows, .packages = c('tidyverse', 'httr'), i = 1:11) %dopar% {
+  get_addr <- function(.base_url) {
+    # print(.base_url)
+    # print('-------')
+    address <- .base_url %>% 
+      # sprintf(x, y) %>% 
+      GET() %>% 
+      .$content %>% 
+      rawToChar() %>% 
+      jsonlite::fromJSON()  %>% 
+      .$address %>% 
+      .$LongLabel
+    
+    if(address %>% is.null()){
+      address <- '-'
+    }
+    # print(address)
+    return(address)
+  }
+    
+  b_tbl_split <- base_tbl %>% 
+    dplyr::filter(grp == i) %>% 
+    dplyr::filter(row_number() < 10)
+
+  result <- b_tbl_split %>% 
+    mutate(addr = map_chr(api_url, get_addr))
+  result
+}
+print(Sys.time() - st)
+}
+
