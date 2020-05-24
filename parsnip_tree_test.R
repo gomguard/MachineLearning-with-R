cfa627fea1ed8cbc8bbe6fe6077d24dfa939a2ab
gomguard <knholic@gmail.com>
2020-05-25 00:08:19 +0900

    추가
    - tidymodels 기초 설명 페이지 추가

--- parsnip_tree_test.R
+++ parsnip_tree_test.R
@@ -7,6 +7,7 @@
 #
 ###########################################################################
 
+# https://rviews.rstudio.com/2019/06/19/a-gentle-intro-to-tidymodels/
 library(tidyverse)
 
 library(parsnip)
