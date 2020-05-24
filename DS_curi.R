d7f493795e9672c169c7bccf6135f72e92a15be2
gomguard <knholic@gmail.com>
2020-05-25 00:28:06 +0900

    수정
    - DS_curi.R
     - 형식 변환
     - ref 추가
     - 내용 추가

---
+++ DS_curi.R
@@ -0,0 +1,89 @@
+###########################################################################
+#
+#    script_name : DS_curi.R
+#    author      : Gomguard
+#    created     : 2020-05-25 00:13:49
+#    description : desc
+#
+###########################################################################
+
+
+데이터 사이언스 관련 강의
+언어
+-	R 기본 & 프로그래밍
+-	Tidyverse 등 패키지 설명
+	Tidyr
+	Dplyr
+	Magrittr
+	Readr
+	Tibble
+	Ggplot2
+	Stringr
+	Purr
+	Lubridate
+	Skimr
+	Recipes
+	Forcats
+	DBI
+	Httr
+	Rvest
+	Xml2
+	Jsonlite
+	Glue
+	Odbc
+	Dbplyr
+	Styler
+	Rsample
+	Parsnip
+	Stats
+	
+-	Sql
+  	기본 언어
+  	R 과 커넥팅 방법
+    	https://rstudio.com/resources/rstudioconf-2020/bridging-the-gap-between-sql-and-r/
+  	Python 과 커넥팅 방법
+기타 프로그램
+-	Db IDE
+-	R IDE, rstudio
+-	Python IDE
+-	…
+기초 통계
+-	기술통계
+분석 알고리즘
+-	supervised
+	Linear regression
+	Linearity
+	다중공선성
+	Logistic
+	Robust
+	Ridge
+	Lasso
+	Elasticnet
+	Svm
+	Tree base
+	Neural net
+
+-	범주화
+	K-means, median, ….
+	Dbscan
+	Hierarchical 
+-	룰베이스
+	Association rules
+
+Web
+-	Encoding
+-	Network base
+
+Etc
+-	Scheduler
+-	Git
+-	Github (action, …..)
+
+
+실전 예제용 강의
+-	특정 사이트 크롤링 및 DB 적재, 스케쥴링
+-	샤이니를 통한 대쉬보드 제작
+-	캐글 데이터 분석
+	EDA
+	상품 추천
+	가격, 수요 예측
