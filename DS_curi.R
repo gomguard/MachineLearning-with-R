6efb4d9829a17b1f1152c46db1c8721d01a08b3c
gomguard <knholic@gmail.com>
2020-05-25 00:50:39 +0900

    추가
    - 내용 추가

--- DS_curi.R
+++ DS_curi.R
@@ -8,7 +8,7 @@
 ###########################################################################
 
 
-데이터 사이언스 관련 강의
+데이터 사이언스 관련 정리
 언어
 -	R 기본 & 프로그래밍
 -	Tidyverse 등 패키지 설명
@@ -46,6 +46,9 @@
   - R 과 커넥팅 방법
     	https://rstudio.com/resources/rstudioconf-2020/bridging-the-gap-between-sql-and-r/
 
+- Database
+
+
 기타 프로그램
   -	Db IDE
   -	R IDE, rstudio
@@ -79,6 +82,8 @@
     - cnn
     - rnn : lstm
   - word2vec
+  - auto encoder
+  - gan
 
 -	범주화
 	- K-means, median, ….
@@ -89,12 +94,27 @@
 	- Association rules
 	- apriori
 
+- time series
+  - arima
+  - gam
+  -
+
+- tf-idf
+  
 - nlp
-  - https://rpubs.com/vsagar19/missing_value_treatment
+  
+모델 검증 및 평가 방법
+- 데이터 선택 방법
+  - hold-out
+  - k-fold
+- 데이터 평가
+  - 정확도 ~ f1 score
+  - roc, auc
 
 결측치 제거 방법
-- amelia
-
+  - amelia
+  - https://rpubs.com/vsagar19/missing_value_treatment
+  
 Web
 -	Encoding
 -	Network base
@@ -104,6 +124,10 @@ Web
   - 공유기
   - ddns
   - ip
+- html
+- css
+- js
+- xpath
 
 Etc
 -	Scheduler
@@ -111,7 +135,8 @@ Etc
 -	Github (action, …..)
 - regexp
 - 데이터 관련 직군 소개	
-
+- R 주석 및 코딩 가이드
+- SQL 코딩 가이드
 
 
 실전 예제용 강의
