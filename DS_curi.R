49e5324d32e56764b0c173f92968cb9f32d745ca
gomguard <knholic@gmail.com>
2020-05-25 00:28:25 +0900

    추가

--- DS_curi.R
+++ DS_curi.R
@@ -12,72 +12,106 @@
 언어
 -	R 기본 & 프로그래밍
 -	Tidyverse 등 패키지 설명
-	Tidyr
-	Dplyr
-	Magrittr
-	Readr
-	Tibble
-	Ggplot2
-	Stringr
-	Purr
-	Lubridate
-	Skimr
-	Recipes
-	Forcats
-	DBI
-	Httr
-	Rvest
-	Xml2
-	Jsonlite
-	Glue
-	Odbc
-	Dbplyr
-	Styler
-	Rsample
-	Parsnip
-	Stats
+  - Tidyr
+  - Dplyr
+  - Magrittr
+  - Readr
+  - Tibble
+  - Ggplot2
+  - Stringr
+  - Purr
+  - Lubridate
+  - Skimr
+  - Recipes
+  - Forcats
+  - DBI
+  - Httr
+  - Rvest
+  - Xml2
+  - Jsonlite
+  - Glue
+  - Odbc
+  - Dbplyr
+  - Styler
+  - Rsample
+  - Parsnip
+  - Stats
+  - base
+  - selenium
+  - phantomjs
+  - arules  
 	
 -	Sql
-  	기본 언어
-  	R 과 커넥팅 방법
+  - 기본 sql
+  - R 과 커넥팅 방법
     	https://rstudio.com/resources/rstudioconf-2020/bridging-the-gap-between-sql-and-r/
-  	Python 과 커넥팅 방법
+
 기타 프로그램
--	Db IDE
--	R IDE, rstudio
--	Python IDE
--	…
+  -	Db IDE
+  -	R IDE, rstudio
+  -	Python IDE
+  -	…
+
 기초 통계
--	기술통계
+  -	평균, 분산, 표준편차
+  - 모평균
+  - 중앙값, mode
+  - T 검정, T 분포
+  - https://www.youtube.com/playlist?list=PLaFfQroTgZnyxGm6hz4lWLaMbslG0KDSG
+  
 분석 알고리즘
 -	supervised
-	Linear regression
-	Linearity
-	다중공선성
-	Logistic
-	Robust
-	Ridge
-	Lasso
-	Elasticnet
-	Svm
-	Tree base
-	Neural net
+  - Linear regression
+    - Linearity
+    - 다중공선성
+  - Logistic
+  - Robust
+  - Ridge
+  - Lasso
+  - Elasticnet
+  - Svm
+  - Tree base
+    - decision tree
+    - bagging : random forest
+    - boosting : xgb, lightgbm
+  - Neural net
+    - dnn
+    - cnn
+    - rnn : lstm
+  - word2vec
 
 -	범주화
-	K-means, median, ….
-	Dbscan
-	Hierarchical 
+	- K-means, median, ….
+	- Dbscan
+	- Hierarchical 
+	
 -	룰베이스
-	Association rules
+	- Association rules
+	- apriori
+
+- nlp
+  - https://rpubs.com/vsagar19/missing_value_treatment
+
+결측치 제거 방법
+- amelia
 
 Web
 -	Encoding
 -	Network base
+  - proxy
+  - domain
+  - port
+  - 공유기
+  - ddns
+  - ip
 
 Etc
 -	Scheduler
 -	Git
 -	Github (action, …..)
+- regexp
+- 데이터 관련 직군 소개	
+
 
 
 실전 예제용 강의
@@ -87,3 +121,6 @@ Etc
 	EDA
 	상품 추천
 	가격, 수요 예측
+	
+
+
