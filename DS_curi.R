06fe3f95ff5312753670d6243de88001d69cc45f
gomguard <knholic@gmail.com>
2020-05-25 17:19:58 +0900

    수정
    - DS_curi.R
     - 기본서 관련 추가
     - dplyr 및 기타 패키지 함수 정리중

--- DS_curi.R
+++ DS_curi.R
@@ -11,12 +11,56 @@
 데이터 사이언스 관련 정리
 언어
 -	R 기본 & 프로그래밍
+- 기본서
+# R for Data Science - https://r4ds.had.co.nz/
+# R for DS Solution - https://jrnold.github.io/r4ds-exercise-solutions/ 
+# ggplot2: Elegant Graphics for Data Analysis - https://ggplot2-book.org/
+# R Packages - https://r-pkgs.org/
+# Advanced R - https://adv-r.hadley.nz/
 -	Tidyverse 등 패키지 설명
   - Tidyr
   - Dplyr
+    - select
+      # https://www.tidyverse.org/blog/2018/06/dplyr-0.7.5/
+    - relocate
+    - rename
+    - rename_with
+      # https://www.tidyverse.org/blog/2020/03/dplyr-1-0-0-select-rename-relocate/
+    - rowwise
+      # https://dplyr.tidyverse.org/dev/articles/rowwise.html
+    - mutate
+    - group_by
+    - group_keys
+    - group_modify (eda용)
+      # https://www.tidyverse.org/blog/2019/06/dplyr-0-8-2/
+    - group_nest
+    - group_split
+    - group_map
+      # https://www.tidyverse.org/blog/2019/02/dplyr-0-8-0/
+      # https://www.tidyverse.org/blog/2018/12/dplyr-0-8-0-release-candidate/
+    - summarise
+    - filter
+    - arrange
+    - row_mutation
+      - rows_update
+      - rows_patch
+      - rows_insert
+      - rows_upsert
+      - rows_delete
+      # https://www.tidyverse.org/blog/2020/05/dplyr-1-0-0-last-minute-additions/#row-mutation
+    - across
+      # https://www.tidyverse.org/blog/2020/04/dplyr-1-0-0-colwise/
+    
+  - tidyselect
+    - any_of, all_of
+    - starts_with, ends_with
   - Magrittr
   - Readr
   - Tibble
+    - enframe = as_tibble
+    - .namerepair
+    - glimpse
+      # https://www.tidyverse.org/blog/2019/01/tibble-2.0.1/
   - Ggplot2
   - Stringr
   - Purr
@@ -30,6 +74,7 @@
   - Xml2
   - Jsonlite
   - Glue
+    # https://www.tidyverse.org/blog/2020/02/glue-strings-and-tidy-eval/
   - Odbc
   - Dbplyr
   - Styler
@@ -116,6 +161,8 @@
   - https://rpubs.com/vsagar19/missing_value_treatment
   
 Web
+# 웹 개발환경 이해
+# https://www.yamestyle.com/201?category=529583
 -	Encoding
 -	Network base
   - proxy
@@ -148,4 +195,28 @@ Etc
 	가격, 수요 예측
 	
 
+# It doesn't work
+# read all csv in the same folder
+# tibble(path = dir(pattern = "\\.csv$")) %>% 
+#   rowwise() %>% 
+#   summarise(read_csv(path))
 
+iris %>% 
+  # dplyr::top_n(n = 10, Petal.Width)
+  top_frac(n = 0.5, Petal.Width)
+
+mtcars %>% 
+  group_by(cyl) %>% 
+  group_split()
+
+mtcars %>% 
+  group_by(cyl, carb) %>% 
+  group_modify(~ head(.x, 1))
+
+# group_by, nest
+mtcars %>% 
+  group_by(cyl) %>% 
+  nest()
+
+mtcars %>% 
+  group_nest(cyl)
