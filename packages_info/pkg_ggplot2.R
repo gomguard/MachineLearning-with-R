f2f5f334da2b8163acb29f345d95ce91143306b2
gomguard <knholic@gmail.com>
2020-05-27 00:05:43 +0900

    수정
    - ggplot 요소 정리

--- packages_info/pkg_ggplot2.R
+++ packages_info/pkg_ggplot2.R
@@ -11,6 +11,18 @@ library(tidyverse)
 
 iris %>% 
   ggplot(aes(Sepal.Length, Sepal.Width, color = Species, size = Sepal.Width)) +
-  geom_point(alpha = 0.5)
+  geom_point(alpha = 0.5) +
+  geom_vline(aes(xintercept = 5))
 
-mtcars
+# 필수 요소
+- 데이터(data)
+- 매핑(mapping)
+- 레이어(layer)
+  - 기하객체(geom)
+
+# 선택요소
+  - 통계객체(stat)
+- 척도(scale)
+- 좌표 객체(coord)
+- 일부단면(facet)
+- 테마(theme)
