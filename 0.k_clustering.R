25b5b6b5e10f5b91a3edb0e487ecec8d25fd6378
gomguard <knholic@gmail.com>
2020-05-28 11:42:07 +0900

    추가
    - k_clustering 파일 신규 추가

---
+++ 0.k_clustering.R
@@ -0,0 +1,91 @@
+#### File Info ###############################
+##
+##  File_name : 0.k_clustering.R
+##  Desc      : Description
+##  Author    : GomGuard
+##  Creted    : 2020-05-28
+## 
+##############################################
+
+## k-means ####
+# install.packages('caret')
+library(caret)
+set.seed(1712)
+
+inTrain <- createDataPartition(y = iris$Species, p = 0.7, list = F)
+training <- iris[inTrain,]
+testing <- iris[-inTrain,]
+
+# scaling
+training.data <- scale(training[-5])
+summary(training.data)
+
+iris.kmeans <- kmeans(training.data[,-5], centers = 3, iter.max = 10000)
+iris.kmeans$centers
+
+training$cluster <- as.factor(iris.kmeans$cluster)
+qplot(Petal.Width, Petal.Length, colour = cluster, data = training)
+table(training$Species, training$cluster)
+
+
+# how to select cores in k-clustering
+# install.packages("NbClust")
+library(NbClust)
+
+nc <- NbClust(training.data, min.nc = 2, max.nc = 15, method = "kmeans")
+
+par(mfrow=c(1,1))
+barplot(table(nc$Best.n[1,]),
+        xlab="Numer of Clusters", ylab="Number of Criteria",
+        main="Number of Clusters Chosen")
+
+
+# how to evaluate accuracy of the model
+# https://ko.wikipedia.org/wiki/K-%ED%8F%89%EA%B7%A0_%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98#Davies-Bouldin_index
+# 1) 군집 내 응집도 최대화(maximizing cohesion within cluster) : 
+#    군집 內 중심(centroid)과 해당 군집의 각 객체 간 거리의 합 최소화
+# 2) 군집 간 분리도 최대화(maxizing separation between clusters) : 
+#    각 군집의 중심(centroid) 間 거리 합 최대화 
+
+
+
+
+
+
+## k-prototype ####
+# install.packages('clustMixType')
+library(clustMixType)
+# generate toy data with factors and numerics
+
+n   <- 100
+prb <- 0.9
+muk <- 1.5 
+clusid <- rep(1:4, each = n)
+
+x1 <- sample(c("A","B"), 2*n, replace = TRUE, prob = c(prb, 1-prb))
+x1 <- c(x1, sample(c("A","B"), 2*n, replace = TRUE, prob = c(1-prb, prb)))
+x1 <- as.factor(x1)
+
+x2 <- sample(c("A","B"), 2*n, replace = TRUE, prob = c(prb, 1-prb))
+x2 <- c(x2, sample(c("A","B"), 2*n, replace = TRUE, prob = c(1-prb, prb)))
+x2 <- as.factor(x2)
+
+x3 <- c(rnorm(n, mean = -muk), rnorm(n, mean = muk), rnorm(n, mean = -muk), rnorm(n, mean = muk))
+x4 <- c(rnorm(n, mean = -muk), rnorm(n, mean = muk), rnorm(n, mean = -muk), rnorm(n, mean = muk))
+
+x <- data.frame(x1,x2,x3,x4)
+
+# apply k prototyps
+kpres <- kproto(x, 4)
+clprofiles(kpres, x)
+
+# in real world  clusters are often not as clear cut
+# by variation of lambda the emphasize is shifted towards factor / numeric variables    
+kpres <- kproto(x, 2)
+clprofiles(kpres, x)
+
+kpres <- kproto(x, 2, lambda = 0.1)
+clprofiles(kpres, x)
+
+kpres <- kproto(x, 2, lambda = 25)
+clprofiles(kpres, x)
