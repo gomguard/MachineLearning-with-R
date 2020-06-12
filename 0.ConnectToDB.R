7665e8c6e9ac6caeed1f0ba03e002bc0bf8a702e
gomguard <34765216+gomguard@users.noreply.github.com>
2020-06-12 10:26:24 +0900

    [기능] Oracle 연동 코드 추가
    Oracle 연동 코드 추가
    tbl 사용 시 추가 설정 필요

--- 0.ConnectToDB.R
+++ 0.ConnectToDB.R
@@ -63,3 +63,31 @@ dbWriteTable(con_post, c("kaggle", "iris"), iris)
 # https://yujuwon.tistory.com/entry/MYSQL-LOAD-DATA-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
 # local-infile
 # 외부 파일 올리기 안될 
+
+
+# Oracle
+# 필요 파일 : jdk, ojdbc
+# jdk - https://openjdk.java.net/
+# ojdbc - repository 내에 업로드 해둠
+
+Sys.setenv(JAVA_HOME='C:/Program Files/JAVA/jdk-14.0.1/')
+library(rJava)
+library(DBI)
+library(RJDBC)
+
+# zip_path <- "C:/Users/knholic/Downloads/DBI_1.1.0.zip"
+# install.packages(zip_path, repos = NULL, type="source")
+
+#driver
+drv <- JDBC("oracle.jdbc.driver.OracleDriver",
+            "C:\\oraclexe\\app\\oracle\\product\\11.2.0\\server\\jdbc\\lib\\ojdbc6.jar")
+
+##DB 연동
+#connection 객체 생성
+conn <- dbConnect(drv, "jdbc:oracle:thin:@//(아이피):(포트번호)/(SID)", "(접속 아이디)", "(접속 비밀번호)")
+
+
+.libPaths()
+sql_translate_env.JDBCConnection <- dbplyr:::sql_translate_env.Oracle
+sql_select.JDBCConnection <- dbplyr:::sql_select.Oracle
+sql_subquery.JDBCConnection <- dbplyr:::sql_subquery.Oracle
