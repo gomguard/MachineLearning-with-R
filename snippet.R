ae38a856cc6e3fc406613233b808f568690cefad
gomguard <34765216+gomguard@users.noreply.github.com>
2020-06-20 17:26:40 +0900

    snippet.R

---
+++ snippet.R
@@ -0,0 +1,10 @@
+
+snippet script_header
+	###########################################################################
+	#
+	#    script_name : ${1:Script Name}
+	#    author      : Gomguard
+	#    created     : `r Sys.time()`
+	#    description : ${2:Script Description}
+	#
+	###########################################################################
